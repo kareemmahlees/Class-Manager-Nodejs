9bcf4c1a60e1f79c65cf38e5166685bd0ba402b6
kareem <kareemmahlees@gmail.com>
2022-11-04 20:05:30 +0200

    plugged in auth routers

--- main.ts
+++ main.ts
@@ -3,12 +3,14 @@ import morgan from "morgan"
 import dotenv from "dotenv"
 import { router as teachersRouter } from "./routers/teacher"
 import { router as studentsRouter } from "./routers/student"
+import { router as authRouter } from "./routers/auth"
 dotenv.config()
 
 const app = express()
 app.use(morgan("dev"))
 app.use("/teachers", teachersRouter)
 app.use("/students", studentsRouter)
+app.use("/auth", authRouter)
 app.use(express.json())
 
 app.get("/", (req, res) => {
